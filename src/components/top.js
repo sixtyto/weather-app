f8b018c0c8d0b68f29ed78eb85dad9b69776c864
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-13 22:47:19 +0100

    top section draft

--- src/components/top.js
+++ src/components/top.js
@@ -6,6 +6,36 @@ export default class TopSection extends React.Component {
     this.state = {};
   }
   render() {
-    return <div className="top-section">Top Section</div>;
+    return (
+      <>
+        <div className="top-section">
+          <div className="top-section__title">Weather App</div>
+          <Weather />
+        </div>
+      </>
+    );
+  }
+}
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div className="top-section__weather">
+          <div className="top-section__weather-header">location</div>
+          <div className="top-section__weather-container">
+            <div className="top-section__weather-container-image">IMG</div>
+            <div className="top-section__weather-container-temperature">
+              Temperature
+            </div>
+          </div>
+          <div className="top-section__weather-footer">Sunny</div>
+        </div>
+      </>
+    );
   }
 }
