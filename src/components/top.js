fd7b2e88230c67e32e1aecd46545019919bc5858
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-13 21:59:34 +0100

    extract components

---
+++ src/components/top.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class TopSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="top-section">Top Section</div>;
+  }
+}
