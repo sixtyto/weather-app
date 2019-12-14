e2afb3264475ea2cc1714ade1410cc9c9d3dd415
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-14 21:05:47 +0100

    Updates

---
+++ precache-manifest.c8a64283a533a58c145336f62fbca4d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6773e202091995363d14d6b1a597114f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "86efe8a86cd9f130434d",
+    "url": "/weather-app/static/css/main.c5002bf3.chunk.css"
+  },
+  {
+    "revision": "043e264ef29ecada9e6f",
+    "url": "/weather-app/static/js/2.a83d16a7.chunk.js"
+  },
+  {
+    "revision": "c763fe6fb41e853f8ea940d27a26578d",
+    "url": "/weather-app/static/js/2.a83d16a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86efe8a86cd9f130434d",
+    "url": "/weather-app/static/js/main.b43419c6.chunk.js"
+  },
+  {
+    "revision": "06bb25e49de1386edcac",
+    "url": "/weather-app/static/js/runtime-main.7fed549c.js"
+  },
+  {
+    "revision": "14a63acfec54873004a1624baeac76ee",
+    "url": "/weather-app/static/media/ice-forest.14a63acf.jpg"
+  }
+]);
